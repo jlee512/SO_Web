ad33e374ded13354abf0f7bf0f6e218a755f48fe
jlee512 <jlee512@aucklanduni.ac.nz>
2017-09-05 15:24:43 +1200

    Datepicker development

---
+++ datepicker_dev.js
@@ -0,0 +1,5 @@
+/**
+ * Created by julia on 5/09/2017.
+ */
+
+var months = [{"name" : "January", "max_days" : 31}, {"name" : "February", "max_days" : 28}, {"name" : "March", "max_days" : 31}, {"name" : "April", "max_days" : 30}, {"name" : "May", "max_days" : 31}, { "name" : "June", "max_days": 30}, { "name" : "July", "max_days" : 31}, { "name" : "August", "max_days" : 31}, { "name" : "September", "max_days" : 30}, { "name" : "October", "max_days" : 31}, { "name" : "November", "max_days" : 30}, { "name" : "December", "max_days" : 31}];
